a7806f85106c3b2b2d2f0897c9bd3c47250d06e7
richky23 <anugerahkencanabirusiak@gmail.com>
2026-06-16 01:47:54 +0700

    Create BerandaLama.js

---
+++ src/components/BerandaLama.js
@@ -0,0 +1,13 @@
+```javascript
+   import React from 'react';
+
+   function BerandaLama() {
+     return (
+       <div className="bangjek-container" style={{ padding: '20px', textAlign: 'center' }}>
+         <h2>Komponen Beranda Lama Berhasil Diamankan!</h2>
+         <p>Tempat penampungan Header, Maps, dan Tombol Armada.</p>
+       </div>
+     );
+   }
+
+   export default BerandaLama;
